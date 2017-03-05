110a6ea560f9944fd1d566e1756c6a61175df88a
siddharth <patelsi14@students.ecu.edu>
2017-03-05 02:23:45 -0500

    AWS Cognito configuration with ShareMe

--- ShareMe/ViewController.swift
+++ ShareMe/ViewController.swift
@@ -7,39 +7,100 @@
 //
 
 import UIKit
+
 import GoogleSignIn
 
-class ViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDelegate {
-   
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        GIDSignIn.sharedInstance().uiDelegate = self
-        GIDSignIn.sharedInstance().delegate = self
-        GIDSignIn.sharedInstance().shouldFetchBasicProfile=true
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+class ViewController: UIViewController, GIDSignInUIDelegate, GIDSignInDelegate, AWSIdentityProviderManager {
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    var googleIdToken = ""
     
+    // Marks:- Google The sign-in flow.
     
- //Marks:- Google Sign in Function
-    public func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+    public func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!){
+
         if (error == nil) {
-            let idToken = user.authentication.idToken
-            let name = user.profile.name
-            let email = user.profile.email
             
-            print(idToken, name, email)
+            googleIdToken = user.authentication.idToken
+           
+            signInToCognito(user: user)
         } else {
+            
             print("\(error.localizedDescription)")
         }
     }
+
+    func logins() -> AWSTask<NSDictionary>{
+
+        let result = NSDictionary(dictionary: [AWSIdentityProviderGoogle: googleIdToken])
+        return AWSTask(result: result)
+    }
+
+    func signInToCognito(user: GIDGoogleUser){
+        let credentialsProvider = AWSCognitoCredentialsProvider(regionType: .USWest2, identityPoolId: "us-west-2:ce587a78-52ec-476e-88d2-994a9001badc", identityProviderManager: self)
+
+        let configuration = AWSServiceConfiguration(region: .USWest2, credentialsProvider: credentialsProvider)
+        
+        AWSServiceManager.default().defaultServiceConfiguration = configuration
+        
+        credentialsProvider.getIdentityId().continueWith(block: {(task:AWSTask) -> AnyObject? in
+        //getIdentityId().continue(  {(task:AWSTask) -> AnyObject? in
+
+            if task.error == nil {
+
+                let syncClient = AWSCognito.default()
+                
+                let dataset = syncClient.openOrCreateDataset("ShareApp")
+
+                dataset.setString(user.profile.email, forKey: "email")
+            
+                dataset.setString(user.profile.name, forKey: "name")
+            
+                let result = dataset.synchronize()
+                result?.continueWith(block: {(task:AWSTask) -> AnyObject? in
+                    if task.error != nil {
+                        print(task.error)
+                    } else {
+                        print(task.result)
+                    }
+
+                    return nil
+                    
+                })
+                
+                return nil
+                
+            }
+
+            if (task.error != nil) {
+                print(task.error)
+                return nil
+            }
+            return nil
+            
+        })
+        
+    }
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().delegate = self
+        GIDSignIn.sharedInstance().shouldFetchBasicProfile = true
+    }
     
     
-
-
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        
+        super.didReceiveMemoryWarning()
+        
+        // Dispose of any resources that can be recreated.
+        
+    }
+    
+    
+    
 }
-
